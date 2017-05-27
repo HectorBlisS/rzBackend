1c5beeac30bf98ce9eeac695c5e7ad1e5297a240
BlisS <contacto@fixter.org>
2017-05-27 11:12:28 -0500

    ready to heroku

--- retoz/settings.py
+++ retoz/settings.py
@@ -173,3 +173,18 @@ CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ORIGIN_WHITELIST = (
 #     'localhost:3000'
 # )
+
+#HEROKU
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
