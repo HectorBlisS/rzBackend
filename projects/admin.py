024a5051a39d5e4f27f5e3efeae9275ff8782a3a
BlisS <contacto@fixter.org>
2017-05-31 12:25:22 -0500

    model fields added

--- projects/admin.py
+++ projects/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Project
 
-admin.site.register(Project)
+class AdminProject(admin.ModelAdmin):
+	list_display = ['name','id','author','goal', 'reached', 'finish']
+	list_filter = ['goal', 'reached', 'finish']
+
+admin.site.register(Project, AdminProject)
--- projects/models.py
+++ projects/models.py
@@ -1,4 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Project(models.Model):
+	author = models.ForeignKey(User, related_name='projects')
 	name = models.CharField(max_length=140)
+	goal = models.DecimalField(decimal_places=2, max_digits=6, blank=True, null=True)
+	description = models.TextField(null=True, blank=True)
+	created = models.DateTimeField(null=True, blank=True)
+	publish = models.DateTimeField(null=True, blank=True)
+	finish = models.DateTimeField(null=True, blank=True)
+	photo = models.ImageField(blank=True, null=True)
+	reached = models.DecimalField(decimal_places=2,  max_digits=6, null=True, blank=True)
+	video = models.URLField(null=True, blank=True)
+	followers = models.ManyToManyField(User, related_name='following')
+
+
+	def __str__(self):
+		return self.name
+
+
