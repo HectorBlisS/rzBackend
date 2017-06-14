c6c494260efe09085366925cced3fb6556334d4a
BlisS <contacto@fixter.org>
2017-06-14 15:41:22 -0500

    endpoints bien sheveres

--- accounts/views.py
+++ accounts/views.py
@@ -10,19 +10,46 @@ from rest_framework import permissions
 
 
 
+class isSelfOrAdmin(permissions.BasePermission):
+    """
+    Global permission check for blacklisted IPs.
+    """
+
+    def has_object_permission(self, request, view, obj):
+    		if request.user.is_superuser or obj.user == request.user:
+    			return True
+
+
+
 class GetMyProfile(APIView):
 	permission_classes = (permissions.IsAuthenticated,)
 	def get(self, request):
 		
 		profile, created = Profile.objects.get_or_create(user=request.user)
-		serializer = ProfileSerializer(profile, many=False)
+		# serializer = ProfileSerializer(profile, many=False)
+		# return Response(serializer.data)
+		serializer = UserSerializer(request.user)
 		return Response(serializer.data)
 
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    #permission_classes = (permissions.IsAuthenticated,)
+    # permission_classes = (permissions.IsAuthenticated, permissions.IsAdminUser)
+    permission_classes = [permissions.AllowAny,]
+
+    def get_permissions(self):
+    	# print('action',self.action)
+    	# print('method',self.request.method)
+    	# print('permiso', self.permission_classes)
+    	# if self.request.method == 'PUT':
+    	if self.action == 'update':
+    		self.permission_classes = [isSelfOrAdmin,]
+    	# else:
+    	# 	self.permissions_classes = None
+    	return super(ProfileViewSet, self).get_permissions()
+
+
 
 
 class UserViewSet(viewsets.ModelViewSet):
