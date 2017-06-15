254e92dcb6316ca185fbff3a3b51b2b28385bb08
BlisS <contacto@fixter.org>
2017-06-15 13:58:11 -0500

    default in manytomany3

--- projects/serializers.py
+++ projects/serializers.py
@@ -21,3 +21,6 @@ class ProjectSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Project
 		fields = '__all__'
+
+	def create(self, validated_data):
+		return Project.objects.create(**validated_data)
