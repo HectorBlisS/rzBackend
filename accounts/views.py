1155d241a07ca2e9fa5e8bb327f7b851b4fc9db3
BlisS <contacto@fixter.org>
2017-06-19 12:53:34 -0500

    rewards

--- accounts/views.py
+++ accounts/views.py
@@ -16,7 +16,7 @@ class isSelfOrAdmin(permissions.BasePermission):
     """
 
     def has_object_permission(self, request, view, obj):
-    		if request.user.is_superuser or obj.user == request.user:
+    		if request.user.is_staff or obj.user == request.user:
     			return True
 
 
--- projects/models.py
+++ projects/models.py
@@ -52,6 +52,10 @@ class Reward(models.Model):
 	title = models.CharField(max_length=140)
 	description = models.CharField(max_length=240)
 	amount = models.DecimalField(decimal_places=2, max_digits=6)
+	date = models.DateField(blank=True, null=True)
+	quantity = models.IntegerField(blank=True, null=True)
+	
+
 
 	def __str__(self):
 		return "{} de {}".format(self.title, self.project)
