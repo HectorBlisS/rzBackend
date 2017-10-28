ac81425f5a4da93468ec44ab44d04edd1bafaa42
BlisS <contacto@fixter.org>
2017-10-27 21:07:29 -0500

    project with details in donacion

--- payments/serializers.py
+++ payments/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Donacion
+from projects.serializers import ProjectSerializer
 
 
 class DonacionSerializer(ModelSerializer):
+	proyecto = ProjectSerializer(many=False, read_only=True)
 	class Meta:
 		model = Donacion
 		fields = "__all__"
