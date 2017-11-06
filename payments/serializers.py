9b4f63dc24f3b0697206a04c5c91bfa30ebe5480
Oswaldinho <oswalfut_96@hotmail.com>
2017-11-06 16:43:09 -0600

    reward serializer in Donacion

--- payments/serializers.py
+++ payments/serializers.py
@@ -7,6 +7,8 @@ from accounts.serializers import UserSerializer
 class DonacionSerializer(ModelSerializer):
 	donador = UserSerializer(many=False, read_only=True)
 	proyecto = ProjectSerializer(many=False, read_only=True)
+	recompensa = RewardSerializer(many=False,read_only=True)
+
 	class Meta:
 		model = Donacion
 		fields = "__all__"
